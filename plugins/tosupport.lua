8abf5b73810537a8a5866c563226d35790d0c1c1
telegrambot81 <telegrambot81@gmail.com>
2016-04-16 17:39:18 +0430

    Added files via upload

---
+++ plugins/tosupport.lua
@@ -0,0 +1,19 @@
+--- https://github.com/amirhmz
+--- https://telegram.me/AmirDark
+do
+
+function run(msg, matches)
+  return " The Support Invition Link : \n https://telegram.me/joinchat/DL4QWz2whWp4OPm_NyBJyg "
+  end
+return {
+  description = "shows support link", 
+  usage = "!tosupport : Return supports link",
+  patterns = {
+    "^([Tt]osupport)$",
+    "^/([Tt]osupport)$"
+  },
+  run = run
+}
+end
+--- https://github.com/amirhmz
+--- https://telegram.me/AmirDark
